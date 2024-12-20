60b175204e4f92372ea37417d5b962e3c387bd02
esyef <yeferson.tech@gmail.com>
2024-12-20 16:44:02 -0500

    Model and menu added to estate.property

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Real Estate Application",
+    "summary": "Real Estate Application",
+    "author": "esyef",
+    "license": "LGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+    "application": True,
+
+}
